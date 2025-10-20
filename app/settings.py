7476e4ae090c2f180ba13dbf2cb90988c1421c7f
EvgheniiC <evghenii.costin@gmx.de>
2025-10-20 17:01:18 +0200

    django-debug-toolbar and db changes

--- app/settings.py
+++ app/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "debug_toolbar",
+
     'main',
     'goods',
 ]
@@ -47,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -119,3 +124,7 @@ STATICFILES_DIRS: list[Path] = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- app/urls.py
+++ app/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, URLResolver
+from django.urls import path, include
+# from debug_toolbar.toolbar import debug_toolbar_urls
 
-urlpatterns:list[URLResolver] = [
+from app.settings import DEBUG
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog')),
 ]
+
+if DEBUG:
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
