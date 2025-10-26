81027196e783450ef6a29744f2cd0524f908660d
EvgheniiC <evghenii.costin@gmx.de>
2025-10-26 12:21:56 +0100

    Templatetags was added

--- main/views.py
+++ main/views.py
@@ -1,13 +1,10 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
-from goods.models import Categories
 
 def index(request: HttpRequest) -> HttpResponse:
-    categories = Categories.objects.all()
     context: dict[str, str] = {
         "tittle": "Home",
         "content": "Main Page Home",
-        "categories": categories
     }
     return render(request, 'main/index.html', context)
 
