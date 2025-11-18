38180ff3940052f4ee57faf4b7a2381f10a51912
EvgheniiC <evghenii.costin@gmx.de>
2025-11-18 15:54:40 +0100

    Postgre sql database connector and Django search

--- app/settings.py
+++ app/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     "debug_toolbar",
 
@@ -76,11 +77,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT':'5432',
     }
 }
 
