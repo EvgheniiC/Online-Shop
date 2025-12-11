ddcaac3d5e5b5cff089e875f7905eea8fe599675
EvgheniiC <evghenii.costin@gmx.de>
2025-12-11 19:08:37 +0100

    registration is ready

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 
 from users.models import User
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class UserLoginForm(AuthenticationForm):
-
     # without django
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
@@ -26,3 +25,25 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+
+    # with backend
+    # first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    # last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+    # password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    # password2 = forms.CharField(
+    #     widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'PASSWORD CONFIRMATION'}))
+    # username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    # email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
